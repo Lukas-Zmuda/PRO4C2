692e8f7f4164e2ec86dd74f387798e053d0fd108
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2021-01-29 09:00:15 +0100

    metody

---
+++ Prvy/src/Metody.java
@@ -0,0 +1,88 @@
+
+public class Metody {
+
+	public static void main(String[] args) {
+
+		//volanie metody
+		pozdravSa();
+		int a = 12;
+		
+		
+		pozdrav("Lukas");
+		
+		kresli("-", 10);
+		
+		System.out.println(druhaMocnina(a));
+		
+		double naDruhu = druhaMocnina(5);
+		
+		System.out.println(naDruhu);
+		
+		System.out.println(ntaMocnina(8, 1));
+		System.out.println(druhaMocnina(12.45));
+		
+		int[] mojePole = vytvorPole(7, -1);
+		vypisPole(mojePole);
+		
+
+	}
+	
+	//metoda bez navratoveho typu
+	static void pozdravSa() {
+		System.out.println("+-----------------------+");
+		pozdrav(" program");
+		//System.out.println("|      Ahoj program     |");
+		System.out.println("+-----------------------+");
+	}
+	
+	static void pozdrav(String meno) {
+		System.out.println("Ahoj " + meno);
+	}
+	
+	static void kresli(String znak, int pocet) {
+		for(int i = 0; i < pocet; i++) {
+			System.out.print(znak);
+		}
+		System.out.println();
+	}
+	
+	static void vypisPole(int[] pole) {
+		for(int prvok: pole) {
+			System.out.print(prvok + ", ");
+		}
+		System.out.println();
+	}
+	//metody s navratovym typom
+	static double druhaMocnina(int cislo) {		
+		double vysledok = 0;
+		vysledok = cislo * cislo;
+		System.out.println("parameter - int");
+		return vysledok;
+	}
+	
+	static double druhaMocnina(double cislo) {
+		double vysledok = 0;
+		vysledok = cislo * cislo;
+		System.out.println("parameter - double");
+		return vysledok;
+	}
+	
+	static double ntaMocnina(int zaklad, int exponent) {
+		double vysledok = 1;
+		
+		for(int i = 0; i < exponent; i++) {
+			vysledok *= zaklad;
+		}
+		return vysledok;
+	}
+	
+	static int[] vytvorPole(int pocetPrvkov, int hodnota) {
+		int[] pole = new int[pocetPrvkov];
+		for(int i = 0; i < pole.length; i++) {
+			pole[i] = hodnota;
+		}
+		return pole;
+	}
+	
+
+}
